9d73c737cb914a9d2e8fc7d911a2e3c76ac77e30
Daniel Harding <daniel.harding@measmerize.com>
2024-04-22 19:02:37 +0100

    Video first push.

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -45,7 +45,6 @@ module.exports = (env, options) => {
       ]
     },
     plugins: [
-      new MiniCssExtractPlugin({ filename: '../css/app.css' }),
       new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
     ]
     .concat(devMode ? [new HardSourceWebpackPlugin()] : [])
